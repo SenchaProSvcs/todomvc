346496053390c608843a72270407a5071d5c54e7
Kevin Cassidy <kevin@overhere.com>
2012-12-20 01:45:47 -0600

    more refactoring/fixes

--- labs/architecture-examples/extjs/js/view/TaskList.js
+++ labs/architecture-examples/extjs/js/view/TaskList.js
@@ -1,70 +1,49 @@
 Ext.define('Ext.ux.DataView.LabelEditor', {
-    extend : 'Ext.Editor',
-    alias: 'widget.todoeditor',
+    extend	: 'Ext.Editor',
+    alias	: 'widget.todoeditor',
 
-    alignment : 'tl-tl',
-    completeOnEnter : true,
-    cancelOnEsc : true,
-    itemSelector : 'label',
-    shim : false,
+    dataIndex: 'label',
+    renderTo: 'kevin', 
 
     autoSize: {
 		width : 'boundEl',
         height : 'boundEl'
     },
 
-    requires: [
-        'Ext.form.field.Text'
-    ],
-
-    constructor: function(config) {
-        config.field = config.field || Ext.create('Ext.form.field.Text', {
-            baseCls:null,
-            cls: 'todo-list',
-            allowOnlyWhitespace: false,
-            selectOnFocus:true
-        });
-        this.callParent([config]);
-    },
-
-    init: function(view) {
-        this.view = view;
-    }
-
+	field: {
+		xtype: 'textfield',
+		allowOnlyWhitespace: false,
+		selectOnFocus: true
+	}
 });
 
 Ext.define('Todo.view.TaskList' , {
-	extend: 'Ext.view.View',
-	alias: 'widget.taskList',
+	extend	: 'Ext.view.View',
+	alias	: 'widget.taskList',
 
-	store: 'Tasks',
-	loadMask: false,
+	store		: 'Tasks',
+	loadMask	: false,
 	itemSelector: 'li',
-    autoEl: 'ul',
 	
-	tpl: Ext.create('Ext.XTemplate',
+	tpl: [ 
        	'<tpl for=".">',
-		'<li class="<tpl if="checked">completed</tpl> <tpl if="editing">editing</tpl>">',
-		'<div class="view">',
-		'<input type="checkbox" class="toggle" <tpl if="checked">checked</tpl> /> ',
-		'<label>{label}</label>',
-		'<a class="destroy"></a>',
-		'</li>',
-        '</div>',
+			'<li class="<tpl if="checked">completed</tpl> <tpl if="editing">editing</tpl>">',
+			'<div class="view">',
+				'<input type="checkbox" class="toggle" <tpl if="checked">checked</tpl> /> ',
+				'<label>{label}</label>',
+				'<a class="destroy"></a>',
+			'</div>',
+			'</li>',
         '</tpl>',
 		{ compiled: true }
-	),
-
-  	plugins: [
-       	Ext.create('Ext.ux.DataView.LabelEditor', {dataIndex: 'label'})
-   	],
+	],
 
 	listeners: {
-		render: function () {
+		render: function (moi) {
 			this.el.on('click', function (clickEvent, el) {
-				var extEl = Ext.get(el)
-				  , parent;
-				parent = extEl.parent('li');
+				var extEl = Ext.get(el),
+					parent = extEl.parent('li');
+
 				this.fireEvent('todoChecked', this.getRecord(parent));
 			}, this, {
 				delegate: 'input.toggle'
@@ -77,7 +56,7 @@ Ext.define('Todo.view.TaskList' , {
 
 				  // Todo this is clearly not the best way to do this, but without this we get an error when
 				  // the item that was clicked is removed.
-				  setTimeout(function () { self.fireEvent('todoRemoveSelected', record); }, 1);
+				  setTimeout(function () { self.fireEvent('removeItem', record); }, 1);
 			}, this, {
 				delegate: 'a'
 			});
