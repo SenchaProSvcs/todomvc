df00000ab480232ac97687cf6fbe01b9444ed621
Kevin Cassidy <kevin@overhere.com>
2013-01-07 21:34:06 -0600

    removed listeners, moved to controller

--- labs/architecture-examples/extjs/js/view/TaskList.js
+++ labs/architecture-examples/extjs/js/view/TaskList.js
@@ -1,65 +1,37 @@
 Ext.define('Ext.ux.DataView.LabelEditor', {
-    extend	: 'Ext.Editor',
-    alias	: 'widget.todoeditor',
+    extend: 'Ext.Editor',
+    alias: 'widget.todoeditor',
 
     dataIndex: 'label',
     renderTo: 'todoapp', 
 
     autoSize: {
-		width 	: 'boundEl',
-        height 	: 'boundEl'
+		width: 'boundEl',
+        height: 'boundEl'
     },
 
 	field: {
-		xtype: 'textfield',
+		xtype : 'textfield',
 		allowOnlyWhitespace: false,
 		selectOnFocus: true
 	}
 });
 
 Ext.define('Todo.view.TaskList' , {
-	extend	: 'Ext.view.View',
-	alias	: 'widget.taskList',
+	extend: 'Ext.view.View',
+	alias: 'widget.taskList',
 
-	store		: 'Tasks',
-	loadMask	: false,
+	store: 'Tasks',
+	loadMask: false,
 	itemSelector: 'li',
-	
-	tpl: [ 
-       	'<tpl for=".">',
-			'<li class="<tpl if="checked">completed</tpl> <tpl if="editing">editing</tpl>">',
-			'<div class="view">',
-				'<input type="checkbox" class="toggle" <tpl if="checked">checked</tpl> /> ',
-				'<label>{label}</label>',
-				'<a class="destroy"></a>',
-			'</div>',
-			'</li>',
-        '</tpl>',
-		{ compiled: true }
-	],
 
-	listeners: {
-		render: function (moi) {
-			this.el.on('click', function (clickEvent, el) {
-				var extEl = Ext.get(el),
-					parent = extEl.parent('li');
-
-				this.fireEvent('todoChecked', this.getRecord(parent));
-			}, this, {
-				delegate: 'input.toggle'
-			});
-
-			this.el.on('click', function (clickEvent, el) {
-				var extEl = Ext.get(el)
-				  , record = this.getRecord(extEl.parent('li'))
-				  , self = this;
-
-				  // Todo this is clearly not the best way to do this, but without this we get an error when
-				  // the item that was clicked is removed.
-				  setTimeout(function () { self.fireEvent('removeItem', record); }, 1);
-			}, this, {
-				delegate: 'a'
-			});
-		}
-	}
+	itemTpl: [ 
+		'<li class="<tpl if="checked">completed</tpl>">',
+		'<div class="view">',
+			'<input type="checkbox" class="toggle" <tpl if="checked">checked</tpl> /> ',
+			'<label>{label}</label>',
+			'<a class="destroy"></a>',
+		'</div>',
+		'</li>'
+	]
 });
